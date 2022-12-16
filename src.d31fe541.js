c03d17ec4df84293f25b0f00bdf7aa70ef34ab5a
Joanna-Jasinska <111195576+Joanna-Jasinska@users.noreply.github.com>
2022-12-16 23:14:36 +0000

    Deploying to gh-pages from @ Joanna-Jasinska/goit-page-template-2022-12-13@0d0727db15394553d6e61259344c616528c28166 🚀

--- src.d31fe541.js
+++ src.d31fe541.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-page-template-2022-12-13/src.584f3ba8.js.map
+//# sourceMappingURL=/goit-page-template-2022-12-13/src.d31fe541.js.map
